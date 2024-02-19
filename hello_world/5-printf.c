303c8531df516ca23cdcb3dd825752c6bcb510b4
SilverDontAsk <steven_rosario@penguin>
2024-02-19 13:47:11 -0400

    Should be fixed now

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Basically just the entry point of the whole program
  *
@@ -12,7 +13,7 @@ int main(void)
 	 * Using printf to print the message followed by a new line
 	 *
 	 */
-	printf("with proper grammar, but the outcome is a piece of art,\n"
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return (0)
+	return (0);
 }
