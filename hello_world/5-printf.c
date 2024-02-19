38c82751709ea7ab5c1ed8465c123ba8fda4caa6
SilverDontAsk <steven_rosario@penguin>
2024-02-19 13:37:03 -0400

    This should print the message by using printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Basically just the entry point of the whole program
+ *
+ * Description: Use printf to output a message this time instead of puts.
+ *
+ * Return: Should always be 0 (Where 0 means it was successful)
+ */
+int main(void)
+{
+	/**
+	 * Using printf to print the message followed by a new line
+	 *
+	 */
+	printf("with proper grammar, but the outcome is a piece of art,\n"
+
+	return (0)
+}
