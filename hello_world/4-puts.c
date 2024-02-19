a5cb631594913bf4f1bb9d4f725486eb30ecba4d
SilverDontAsk <steven_rosario@penguin>
2024-02-19 13:26:16 -0400

    surely it wasnt that random extra space

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -7,12 +7,12 @@
  *
  * Return: Always 0 (0=sucess)
  */
-	int main(void) 
-	{
-		/**
-		 * Puts is another way to print the message
-		 */
-		puts("\"Programming is like building a multilingual puzzle");
-		
-		return (0);
+int main(void)
+{
+	/**
+	 * Puts is another way to print the message
+	 */
+	puts("\"Programming is like building a multilingual puzzle");
+	
+	return (0);
 }
