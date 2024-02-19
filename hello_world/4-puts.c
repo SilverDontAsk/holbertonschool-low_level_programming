c952c34011e84ab342d314aa8508825a3fcb7628
SilverDontAsk <steven_rosario@penguin>
2024-02-19 13:07:55 -0400

    Shorely

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -11,7 +11,7 @@ int main(void)
 	/**
 	 * Puts is another way to print the message
 	 */
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle"\n);
 
 	return (0);
 }
