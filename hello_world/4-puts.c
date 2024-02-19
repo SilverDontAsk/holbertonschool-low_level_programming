0ecf65fcb470980c458265b6b036dd4690543b07
SilverDontAsk <steven_rosario@penguin>
2024-02-19 13:06:00 -0400

    Should hopefully now be in Betty coding style if I managed to understand everything right

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
-
-int main() {
+/**
+ * main - This would be the entry point of the program
+ *
+ * Description: Basically print a message by using puts instead of printf
+ *
+ * Return: Always 0 (0=sucess)
+ */
+int main(void) 
+{
+	/**
+	 * Puts is another way to print the message
+	 */
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
