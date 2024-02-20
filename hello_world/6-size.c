82510542ac3658a0f6a1116a48fb25b6d4bf1905
SilverDontAsk <steven_rosario@penguin>
2024-02-20 10:01:54 -0400

    This should fix the betty issues

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -3,23 +3,21 @@
 /**
  * main - just the entry point for the program
  *
- * Description: Print the size of various types on the computer it is compiled and run on
+ * Description: Print byte values of various type on the hardware it is run on
  *
  * Return: 0 (If 0= success)
  */
 int main(void)
 
 {
-/**
- *
- * Using a metric load of printf to print out various types
- *
- */
-printf("Size of a char: %lu\n byte(s)", sizeof(char));
-printf("Size of an int: %lu\n byte(s)", sizeof(int));
-printf("Size of a long int: %lu\n byte(s)", sizeof(long int));
-printf("Size of a long long int: %lu\n byte(s)", sizeof(long long int));
-printf("Size of a float: %lu\n byte(s)", sizeof(float));
+	/**
+	* Using printf to print byte values of various types
+	*/
+	printf("Size of a char: %lu\n byte(s)", sizeof(char));
+	printf("Size of an int: %lu\n byte(s)", sizeof(int));
+	printf("Size of a long int: %lu\n byte(s)", sizeof(long int));
+	printf("Size of a long long int: %lu\n byte(s)", sizeof(long long int));
+	printf("Size of a float: %lu\n byte(s)", sizeof(float));
 
-return (0);
+	return (0);
 }
