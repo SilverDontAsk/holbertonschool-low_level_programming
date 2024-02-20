7f7a8c277f031e1860126812b9c33e10d78f57e7
SilverDontAsk <steven_rosario@penguin>
2024-02-20 09:38:33 -0400

    I was indeed missing something, I was formatting it for an unsigned int instead of a long unsigned int

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -15,10 +15,11 @@ int main(void)
 	 * Using a metric load of printf to print out various types
 	 *
 	 */
-	printf("Size of a char: %u\n byte(s)"; sizeof(char));
-	printf("Size of an int: %u\n byte(s)"; sizeof(int));
-	printf("Size of a long int: %u\n byte(s)"; sizeof(long int));
-	printf("Size of a long long int: %u\n byte(s)"; sizeof(long long int));
-	printf("Size of a float: %u\n byte(s)"; sizeof(float));
+	printf("Size of a char: %lu\n byte(s)", sizeof(char));
+	printf("Size of an int: %lu\n byte(s)", sizeof(int));
+	printf("Size of a long int: %lu\n byte(s)", sizeof(long int));
+	printf("Size of a long long int: %lu\n byte(s)", sizeof(long long int));
+	printf("Size of a float: %lu\n byte(s)", sizeof(float));
 
 	return (0);
+}
