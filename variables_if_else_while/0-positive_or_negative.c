6f9e59e9e1c424747046d4213adbc2798e976f07
SilverDontAsk <steven_rosario@penguin>
2024-02-20 20:59:42 -0400

    I assume that if im right this is correct in its formatting

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,29 +3,26 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there 
+/*
+ * main - entry point
  *
- * main - entry point of the program
+ * Description - program to describe n as +,- or =
  *
- * Description basically adding code to a source code to say if n is greater, lower or equal to 0
+ * Return 0 (If 0 then success)
  *
- * Return 0 (If 0 then success) 
- *
- * */
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there 
-	 *
+	/*
 	 * if implies that if n is bigger than 0, its positive
 	 *
-	 * else if here would imply that if n isnt greater or lower than zero, its zero
+	 * else if is here to say that is n is 0, to say its zero
 	 *
 	 * else would imply that if n isnt positive, its negative
-	 *
 	 */
 	if (n > 0)
 	{
