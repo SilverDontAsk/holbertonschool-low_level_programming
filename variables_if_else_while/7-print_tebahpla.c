7d14fcca9072b02aa3158af0e52279070eb57d40
SilverDontAsk <steven_rosario@penguin>
2024-02-21 13:37:03 -0400

    ok

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - use putchar to output a to z in lowercase
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+char ch = 'z';
+for (ch = 'z'; ch => 'a'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
