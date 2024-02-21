2803f9c9cfc5f4095fb8671c091ea7f77830ff8f
SilverDontAsk <steven_rosario@penguin>
2024-02-21 10:17:01 -0400

    Need to see errors

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,17 +13,17 @@ int main(void)
 	/* your code goes there */
 	lastDigit = n % 10;
 
-	{ if (n > 0)
+	if (n > 0)
+	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
-
 	}
-	{ else if ( n == 0)
-		printf("Last digit of %d is %d and is 0\n", n);
-
+	else if ( n == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 	}
-	{ else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n);
-
+	 else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 	}
 	return (0);
 }
