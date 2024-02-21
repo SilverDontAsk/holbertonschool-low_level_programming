c2b0a40bac313d2b6a234100c10a56a6d54508d2
SilverDontAsk <steven_rosario@penguin>
2024-02-21 13:41:13 -0400

    seeing if there's anything more wrong

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main - use putchar to output a to z in lowercase
+ * main - use putchar to output the alphabet in reverse
  *
  * Return: always 0
  */
 int main(void)
 {
 char ch = 'z';
-for (ch = 'z'; ch >= 'a'; ch++)
+for (ch = 'z'; ch >= 'a'; --ch)
 putchar(ch);
 putchar('\n');
 return (0);
