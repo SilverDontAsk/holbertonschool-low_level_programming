dd1403eb67d810835abe13c25ef26aea5cf8e952
SilverDontAsk <steven_rosario@penguin>
2024-02-21 12:30:24 -0400

    Im starting to hate betty

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,14 +3,28 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - entry point of the program
+ *
+ * Description - finding the last digit of a random number
+ *
+ * Return: 0 (If 0, success)
+ */
 int main(void)
 {
 int n, lastDigit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-/* your code goes there */
+/**
+ * if - finds if last digit is bigger than 5
+ *
+ * else if 1 - finds if lastdigit = 0
+ *
+ * else if 2 -  finds if lastdigit is less than 6 but not 0
+ * lastDigit: calculate the last digit of n
+ */
 lastDigit = n % 10;
+
 if (lastDigit > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
