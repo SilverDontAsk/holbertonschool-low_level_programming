a531d11233d5b83025f3371cd1408071ef37c277
SilverDontAsk <steven_rosario@penguin>
2024-02-21 09:44:15 -0400

    I wanna see the errors

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n, lastDigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	lastDigit = n % 10;
+
+	{ if (n > 0)
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+
+	}
+	{ else if ( n == 0)
+		printf("Last digit of %d is %d and is 0\n", n);
+
+	}
+	{ else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n);
+
+	}
+	return (0);
+}
