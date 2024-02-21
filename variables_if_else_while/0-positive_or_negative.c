30c142ddc41c3439f192da372c95ca49d811a160
SilverDontAsk <steven_rosario@penguin>
2024-02-20 20:45:04 -0400

    I believe this should be close to right

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,15 +3,31 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/* betty style doc for function main goes there 
+ *
+ * main - entry point of the program
+ *
+ * Description basically adding code to a source code to say if n is greater, lower or equal to 0
+ *
+ * Return 0 (If 0 then success) 
+ *
+ * */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n >= 0)
+	/* your code goes there 
+	 *
+	 * if implies that if n is bigger than 0, its positive
+	 *
+	 * else if here would imply that if n isnt greater or lower than zero, its zero
+	 *
+	 * else would imply that if n isnt positive, its negative
+	 *
+	 */
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	} else if (n == 0)
