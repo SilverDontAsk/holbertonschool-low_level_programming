1694302b3f02205e2e437d72af47b4d7b933a7a6
SilverDontAsk <steven_rosario@penguin>
2024-02-22 13:03:45 -0400

    Im using ascii code to translate to text

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -7,7 +7,23 @@
  */
 int main(void)
 {
-printf("_putchar\n");
+int ascii_code1 = 95;
+int ascii_code2 = 112;
+int ascii_code3 = 117;
+int ascii_code4 = 116;
+int ascii_code5 = 99;
+int ascii_code6 = 104;
+int ascii_code7 = 97;
+int ascii_code8 = 114;
 
+putchar(ascii_code1);
+putchar(ascii_code2);
+putchar(ascii_code3);
+putchar(ascii_code4);
+putchar(ascii_code5);
+putchar(ascii_code6);
+putchar(ascii_code7);
+putchar(ascii_code8);
+putchar('\n');
 return (0);
 }
