119efd716704d3613566c81aa23fe5f28814ab52
SilverDontAsk <steven_rosario@penguin>
2024-02-23 12:07:55 -0400

    idk

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - print out the alphabet
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+print_alphabet();
+{
+_putchar(char *);
+_putchar('\n');
+}
+return (0);
+}
