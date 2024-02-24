fed170471a157ae1944470ab5af2fa5bcec52af8
SilverDontAsk <steven_rosario@penguin>
2024-02-24 18:39:57 -0400

    Surely

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+}
+else if (n == 0)
+{
+return (0);
+}
+else
+{
+return (-1);
+}
+}
