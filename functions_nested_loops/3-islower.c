8e672871baa10ecd1a3e895d90c048706cc2c7d9
SilverDontAsk <steven_rosario@penguin>
2024-02-24 17:54:46 -0400

    This should return 1 if c is lowercase and 0 if otherwise

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,33 +2,12 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int main(void)
-{
-	int r;
+int _islower(int c)
 
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-        _putchar(r + '0');
-	r = _islower('c');
-        _putchar(r + '0');
-	r = _islower('I');
-        _putchar(r + '0');
-	r = _islower('s');
-        _putchar(r + '0');
-	r = _islower(70);
-        _putchar(r + '0');
-	r = _islower(20);
-        _putchar(r + '0');
-	r = _islower('U');
-        _putchar(r + '0');
-	r = _islower('n');
-        _putchar(r + '0');
-	r = _islower(0);
-        _putchar(r + '0');
-	r = _islower('H');
-        _putchar(r + '0');
-_putchar('\n');
-return (0);
+{
+if (c >= 20  && c <= 108)
+{
+return(1);
 }
-	
+return(0);
+}
