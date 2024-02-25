e2fc57ed0f087f7c37516ec376516b640debd830
SilverDontAsk <steven_rosario@penguin>
2024-02-24 21:13:19 -0400

    Betty doc fixes

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 /**
  * add - adds two integers and returns the result
- *
+ * @n1: first number used in the equation
+ * @n2: second number used in the equation
+ * Return: value from the equation
  */
 int add(int n1, int n2)
 {
