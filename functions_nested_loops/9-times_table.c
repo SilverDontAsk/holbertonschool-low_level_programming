4caad277854c4a61db2ceed3bc7d1569dece1c37
SilverDontAsk <steven_rosario@penguin>
2024-02-24 21:04:38 -0400

    this one left me more confused, had a hard time getting the numbers to form an actual cube

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,36 +6,33 @@
  */
 void times_table(void)
 {
-    int n, k;
+int n, k, r, m, l;
 
-    for (n = 0; n <= 9; n++)
-    {
-        for (k = 0; k <= 9; k++)
-        {
-            int r = n * k;
-
-            if (r < 10 && k != 0)
-            {
-                _putchar(' ');
-            }
-
-            if (k != 0)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-
-            if (r < 10)
-            {
-                _putchar(' ');
-            }
-
-            if (r >= 10)
-            {
-                _putchar(r / 10 + '0');
-            }
-            _putchar(r % 10 + '0');
-        }
-        _putchar('\n');
-    }
+for (n = 0; n <= 9; n++)
+{
+for (k = 0; k <= 9; k++)
+{
+r = n * k;
+if (r > 9)
+{
+l = r % 10;
+m = (r - l) / 10;
+_putchar(44);
+_putchar(32);
+_putchar(m + '0');
+_putchar(l + '0');
+}
+else
+{
+if (k != 0)
+{
+_putchar(44);
+_putchar(32);
+_putchar(32);
+}
+_putchar(r + '0');
+}
+}
+_putchar('\n');
+}
 }
