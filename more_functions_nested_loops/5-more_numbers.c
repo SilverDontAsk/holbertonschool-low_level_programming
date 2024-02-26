7100c3cdbc12bae0ce42bb5d7815d6b9dcbb9ccd
SilverDontAsk <steven_rosario@penguin>
2024-02-26 12:52:56 -0400

    this should print 0 to 14 10 times

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,13 +5,17 @@
  */
 void more_numbers(void)
 {
-	int i, ch;
-	for (i = 0; i <= 10; i++)
+int i, ch;
+for (i = 0; i < 10; i++)
+{
+	for (ch = 0; ch <= 14; ch++)
 	{
-		for (ch = 0; ch <= 14; ch++)
-		{
-			_putchar(ch + '0');
-		}
-		_putchar('\n');
+		if (ch > 9)
+	{
+			_putchar((ch / 10) + '0');
 	}
+			_putchar((ch % 10) + '0');
+	}
+_putchar('\n');
+}
 }
