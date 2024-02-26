f1ccd9a9d9400f250dda840497ce4514d1603b27
SilverDontAsk <steven_rosario@penguin>
2024-02-26 13:09:44 -0400

    This prints a line based on n

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -5,17 +5,17 @@
  */
 void print_line(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		int l;
-		for (l = 0; l < n; l++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
-	}
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int l;
+for (l = 0; l < n; l++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
 }
