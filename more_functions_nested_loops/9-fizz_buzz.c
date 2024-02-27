9ae95b7c75e798731d7bc817b6e1e4cc93304831
SilverDontAsk <steven_rosario@penguin>
2024-02-27 11:19:09 -0400

    This should be right now

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,21 +12,25 @@ int n;
 n = 100;
 for (n = 1; n <= 100; n++)
 {
-	if (n % 3 == 0 && n % 5 ==0)
-	{
-		printf("FizzBuzz ");
-	}
-	else if (n % 3 == 0)
-	{
-		printf("Fizz ");
-	}
-	else if (n % 5 == 0)
-	{
-		printf("Buzz ");
-	}
-	else
-	{
-	printf("%d ", n);
+if (n % 3 == 0 && n % 5 ==0)
+{
+printf("FizzBuzz");
+}
+else if (n % 3 == 0)
+{
+printf("Fizz");
+}
+else if (n % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", n);
+}
+if (n < 100)
+{
+printf(" ");
 }
 }
 return (0);
