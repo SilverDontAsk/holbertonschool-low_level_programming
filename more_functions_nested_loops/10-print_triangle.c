fa57855d16ec4c4448be78e392904fdd2dcc7f62
SilverDontAsk <steven_rosario@penguin>
2024-02-27 14:44:57 -0400

    This should work

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,21 +1,30 @@
 #include "main.h"
 /**
- * print_triangle - prints a triangle
+ * print_triangle - prints a left angle triangle
  * @size: size of the triangle
+ * Return: nothing
  */
-void print_triangle(int size)
+
+void print_triangle(int size) 
 {
-	int l, h;
-	for (l = 1; l <= size; ++l)
+int l, h, j;
+if (size <= 0)
+{
+	_putchar('\n');
+}
+else
+{
+	for (l = 0; l < size; l++)
 	{
-		for (h = l; h <= size; ++h)
+		for (h = size - l; h > 1; h--)
 		{
 			_putchar(' ');
 		}
-		for (h = 1; h <= l; h++)
+		for (j = 0; j <= l; j++)
 		{
 			_putchar('#');
 		}
 		_putchar('\n');
 	}
+}
 }
