cdcca7db35cf9a4e2c1eefbff26ab56a16ed4459
SilverDontAsk <steven_rosario@penguin>
2024-02-27 11:09:05 -0400

    This should be right

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ * @f: fizz if multiple of 3
+ * @b: buzz if multiple of 5
+ * @g: fizzbuzz if both
+ * Return: always 0
+ */
+int main(void)
+{
+int n;
+n = 100;
+for (n = 1; n <= 100; n++)
+{
+	printf(" ");
+	if (n % 3 == 0 && n % 5 ==0)
+	{
+		printf("FizzBuzz ");
+	}
+	else if (n % 3 == 0)
+	{
+		printf("Fizz ");
+	}
+	else if (n % 5 == 0)
+	{
+		printf("Buzz ");
+	}
+	else
+	{
+	printf("%d", n);
+}
+}
+return (0);
+}
