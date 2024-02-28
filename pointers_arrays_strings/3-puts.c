257444b02d12dfd8e082871fa46d031437d5df0d
SilverDontAsk <steven_rosario@penguin>
2024-02-28 13:55:45 -0400

    idk

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _puts - prints a string followed by newline
  * @str: string to be printed
@@ -8,8 +9,8 @@ void _puts(char *str)
 {
 while (*str != '\0')
 {
-_putchar(*str);
+putchar(*str);
 str++;
 }
-_putchar('\n');
+putchar('\n');
 }
