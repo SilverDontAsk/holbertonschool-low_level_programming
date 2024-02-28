6764c7fa4ac642b76bcd7b9f03755839eda10956
SilverDontAsk <steven_rosario@penguin>
2024-02-28 09:20:24 -0400

    This should work

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -4,27 +4,26 @@
  * @size: size of the triangle
  * Return: nothing
  */
-
-void print_triangle(int size) 
+void print_triangle(int size)
 {
 int l, h, j;
 if (size <= 0)
 {
-	_putchar('\n');
+_putchar('\n');
 }
 else
 {
-	for (l = 0; l < size; l++)
-	{
-		for (h = size - l; h > 1; h--)
-		{
-			_putchar(' ');
-		}
-		for (j = 0; j <= l; j++)
-		{
-			_putchar('#');
-		}
-		_putchar('\n');
-	}
+for (l = 0; l < size; l++)
+{
+for (h = size - l; h > 1; h--)
+{
+_putchar(' ');
+}
+for (j = 0; j <= l; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
 }
 }
