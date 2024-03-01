4c0a6d726dbfbcd5a25e7d69ee256b571e4aeed7
SilverDontAsk <steven_rosario@penguin>
2024-03-01 18:56:34 -0400

    Betty fixes and writing fixes

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -3,14 +3,14 @@
  * _strcmp - compares two strings
  * @s1: string 1
  * @s2: string 2
- * Return:
+ * Return: comparison between string 1 and 2
  */
 int _strcmp(char *s1, char *s2)
 {
-	while ( *s1 && *s2 && *s1 == *s2)
+	while (*s1 && *s2 && *s1 == *s2)
 	{
 		s1++;
 		s2++;
 	}
-	return (s1 - s2);
+	return (*s1 - *s2);
 }
