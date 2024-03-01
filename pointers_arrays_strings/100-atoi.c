6f572311de26e9cf1ab6cc224fb9db43177f052a
SilverDontAsk <steven_rosario@penguin>
2024-03-01 14:03:39 -0400

    Betty fixes

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,23 +2,21 @@
 #include <limits.h>
 #include <stdio.h>
 /**
- * atoi - converts string to integer
+ * _atoi - converts string to integer
  * @s: string to be converted
  * Return: converted integers
  */
 int _atoi(char *s)
 {
-	int sig = 1;
-	unsigned int n = 0;
-	do
-	{
-		if (*s == '-')
-			sig *= -1;
-		else if(*s >= '0' && *s <= '9')
-			n = (n * 10) + (*s - '0');
-		else if (n > 0)
-			break;
-	}
-	while (*s++);
-	return (n * sig);
+int sig = 1;
+unsigned int n = 0;
+do {
+if (*s == '-')
+sig *= -1;
+else if (*s >= '0' && *s <= '9')
+n = (n * 10) + (*s - '0');
+else if (n > 0)
+break;
+} while (*s++);
+return (n *sig);
 }
