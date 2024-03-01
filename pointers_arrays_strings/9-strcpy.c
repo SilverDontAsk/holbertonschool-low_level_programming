336a67daa3a68f148fe7002c5fd0e4b40b645c83
SilverDontAsk <steven_rosario@penguin>
2024-02-29 22:22:03 -0400

    Weird lil FF at the beginning but let's see

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcpy - copies string pointed to by src to dest
+ * @src: string source
+ * @dest: destination
+ * return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+_putchar(*dest);
+return (dest);
+}
