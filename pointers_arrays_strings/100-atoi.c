744a1c784a24375bb8922df44161b3970a889dfc
SilverDontAsk <steven_rosario@penguin>
2024-03-01 14:00:57 -0400

    I need to learn to use do

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <limits.h>
+#include <stdio.h>
+/**
+ * atoi - converts string to integer
+ * @s: string to be converted
+ * Return: converted integers
+ */
+int _atoi(char *s)
+{
+	int sig = 1;
+	unsigned int n = 0;
+	do
+	{
+		if (*s == '-')
+			sig *= -1;
+		else if(*s >= '0' && *s <= '9')
+			n = (n * 10) + (*s - '0');
+		else if (n > 0)
+			break;
+	}
+	while (*s++);
+	return (n * sig);
+}
