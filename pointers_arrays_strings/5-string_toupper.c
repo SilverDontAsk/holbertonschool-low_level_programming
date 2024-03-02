d92eae4cdaaa2fa6b26723f89919fc8b587be575
SilverDontAsk <steven_rosario@penguin>
2024-03-02 13:49:39 -0400

    Betty fixes

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * string_toupper - changes lowercase to uppercase
- * @*: string to be changed
+ * @str: string to be changed
  * Return: uppercase string
  */
 char *string_toupper(char *str)
 {
 char *ptr = str;
-while (*ptr !='\0')
+while (*ptr != '\0')
 {
 	if (*ptr >= 'a' && *ptr <= 'z')
 	{
