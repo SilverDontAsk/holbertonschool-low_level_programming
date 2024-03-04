906f3237e4e0ef731020610d6b13816c4ef5c126
SilverDontAsk <steven_rosario@penguin>
2024-03-04 14:17:14 -0400

    This should fix it

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,14 +1,33 @@
 #include "main.h"
 /**
  * _strspn - gets the length of the prefix substring
- * @s: string 
+ * @s: string
  * @accept: only the bytes of this should be read
  * Return: number of bytes in initial segment of s that have bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
 int i = 0;
-while (*s && _strchr(accept, *s++))
-	i++;
-return (i);
+unsigned int c = 0;
+const char *ps;
+while (*s)
+{
+	i = 0;
+	ps = accept;
+	while (*ps)
+	{
+		if (*ps == *s)
+		{
+			i = 1;
+			break;
+		}
+		ps++;
+	}
+	if (!i)
+		break;
+	c++;
+	s++;
 }
+return (c);
+}
+
