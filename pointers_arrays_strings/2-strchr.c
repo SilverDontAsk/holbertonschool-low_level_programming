734257a26bae32a84106b87d6b35367162df0745
SilverDontAsk <steven_rosario@penguin>
2024-03-04 13:21:17 -0400

    Betty fixes

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -3,15 +3,13 @@
  * _strchr - locates a character in a string
  * @s: string
  * @c: character to be found
- * Return: pointer to the first occurence of character c in s, null if not found
+ * Return: pointer to first occurence of character c in s, null if not found
  */
 char *_strchr(char *s, char c)
 {
-do
-{
+do {
 if (*s == c)
 return (s);
-}
-while (*s++);
+} while (*s++);
 return ('\0');
 }
