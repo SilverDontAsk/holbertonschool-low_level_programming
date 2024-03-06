e49ba90c0314dce57d360aa2132c2031cb626e36
SilverDontAsk <steven_rosario@penguin>
2024-03-06 12:53:18 -0400

    This a headache

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,19 +1,34 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * is_prime_number - finds if number is a prume number
+ * prime_recursive - checks if number is prime
+ * @n: number to check
+ * @divi: divisor tested
+ * Return: 1 if prime, 0 if not
+ */
+int prime_recursion(int n, int divi);
+/**
+ * is_prime_number - finds if number is a prime number
  * @n: number to search
  * Return: 1 if prime, 0 if not
  */
 int is_prime_number(int n)
 {
-	int i;
+        return prime_recursion(n, 2);
+}
+/**
+ * prime_recursive - checks if number is prime
+ * @n: number to check
+ * @divi: divisor tested
+ * Return: 1 if prime, 0 if not
+ */
+int prime_recursion(int n, int divi)
+{
 	if (n <= 1)
 		return (0);
-	for (i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-			return (0);
-	}
-	return (1);
+	if (divi * divi > n)
+		return (1);
+	if (n % divi == 0)
+		return (0);
+	return prime_recursion(n, divi + 1);
 }
