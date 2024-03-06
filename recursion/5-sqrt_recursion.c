dcf71070a4787b3b578d019b16b9c97cf8562300
SilverDontAsk <steven_rosario@penguin>
2024-03-06 12:24:13 -0400

    Code fixes and rework

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,43 +1,27 @@
 #include "main.h"
+#include <stdio.h>
+int squarert(int n, int i);
 /**
- * sqrt_recursion - function to help find the square root recursively
- * @start: lower bound of search
- * @end: upper bound of search
- * @n: number to find to square root of
- * Return: square root of n
- */
-int sqrt_recursion(int n, int start, int end)
-{
-int mid = start + (end - start) / 2;
-long square;
-if (n < 0)
-{
-return (-1);
-}
-if (start > end)
-{
-return (-1);
-}
-square = mid * mid;
-if (square == n)
-{
-return (mid);
-}
-else if (square < n)
-{
-return (sqrt_recursion(n, mid + 1, end));
-}
-else
-{
-return (sqrt_recursion(n, start, mid - 1));
-}
-}
-/**
- * _sqrt_recursion - returns natural square root
- * @n: number to find the square root of
+ * _sqrt-recursion - returns natural square root of n
+ * @n: number to find square root of
  * Return: square root of n
  */
 int _sqrt_recursion(int n)
 {
-return (sqrt_recursion(n, 0, n));
+	return (squarert(n, 1));
+}
+/**
+ * squarert - calculates the natural square root
+ * @n: number to calculate
+ * @i iterate number
+ * Return: natural square root
+ */
+int squarert(int n, int i)
+{
+int root = i * i;
+if (root > n)
+return (-1);
+if (root == n)
+return (i);
+return (squarert(n, i + 1));
 }
