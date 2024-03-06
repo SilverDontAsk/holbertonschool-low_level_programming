ced462e21541a430ec3305ec5def5e670c4f6bb6
SilverDontAsk <steven_rosario@penguin>
2024-03-06 11:11:56 -0400

    Bett fixes

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- *
- *
- *
+ * _pow_recursion - raise x to the power y
+ * @x: number to be raised
+ * @y: number to raise by
+ * Return: result of raising x to the power of y
  */
 int _pow_recursion(int x, int y)
 {
