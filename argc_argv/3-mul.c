9e744bfe6e2d4ec05faa6fd7c538d571f693c4ea
SilverDontAsk <steven_rosario@penguin>
2024-03-08 14:46:00 -0400

    Betty fix

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -4,19 +4,19 @@
  * main - multiplies two numbers
  * @argc: argument count
  * @argv: argument vector
- * Return: 0 
+ * Return: 0
  */
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-	int n1, n2, res;
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[2]);
-	res = n1 * n2;
-	printf("%d\n", res);
-	return (0);
+int n1, n2, res;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+n1 = atoi(argv[1]);
+n2 = atoi(argv[2]);
+res = n1 *n2;
+printf("%d\n", res);
+return (0);
 }
