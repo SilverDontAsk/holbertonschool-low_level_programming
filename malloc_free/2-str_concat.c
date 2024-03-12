c1b1393b4e5cf85ad21fb9530fd84d969bc3577c
SilverDontAsk <steven_rosario@penguin>
2024-03-12 09:54:13 -0400

    idk

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,16 +14,22 @@ int lens2;
 int i;
 char *res;
 while (s1 && s1[lens1])
-	lens1++;
+{
+lens1++;
+}
 while (s2 && s2[lens2])
-	lens2++;
+{
+lens2++;
+}
 res = malloc(sizeof(char) * (lens1 + lens2 +1));
 if (res == NULL)
-	return (NULL);
+{
+return (NULL);
+}
 for (i = 0; i < lens1; i++)
-	res[i] = s1[i];
+res[i] = s1[i];
 for (i = 0; i < lens2; i++)
-	res[lens1 + i] = s2[i];
+res[lens1 + i] = s2[i];
 res[lens1 + lens2] = '\0';
 return (res);
 }
