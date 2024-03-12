7ea01c636ed094e419a251f42e74d5c0ab50d3ca
SilverDontAsk <steven_rosario@penguin>
2024-03-12 11:58:14 -0400

    idk

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -9,29 +9,31 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-int lens1;
-int lens2;
-int i, p;
-char *res;
-res = malloc(sizeof(char) * (lens1 + lens2 + 1));
-if (s1 != NULL)
-{
-while (s1[lens1])
-lens1++;
-}
-if (s2 != NULL)
-{
-while (s2[lens2])
-lens2++;
-}
-if (res == NULL)
-{
-return (NULL);
-}
-for (i = 0; i < lens1; i++)
-res[i] = s1[i];
-for (p = 0; p < lens2; p++)
-res[lens1 + p] = s2[p];
-res[lens1 + lens2] = '\0';
-return (res);
+int len_s1, len_s2;
+char *result;
+int i, j;
+    if (s1 == NULL || s2 == NULL) {
+        return (NULL);
+    }
+    len_s1 = 0;
+    while (s1[len_s1] != '\0') {
+        len_s1++;
+    }
+    len_s2 = 0;
+    while (s2[len_s2] != '\0') {
+        len_s2++;
+    }
+result = (char *)malloc((len_s1 + len_s2 + 1) * (sizeof(char)));
+    if (result == NULL) {
+        return NULL;
+    }
+    for (i = 0; i < len_s1; i++) {
+        result[i] = s1[i];
+    }
+    for (j = 0; j < len_s2; j++) {
+        result[i + j] = s2[j];
+    }
+    result[i + j] = '\0';
+
+    return (result);
 }
