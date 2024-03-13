a85a2bb6aa4dcd438c4d2ab818cef86c6c0a131d
SilverDontAsk <steven_rosario@penguin>
2024-03-13 13:24:55 -0400

    This works on terminal so it should be good

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * array_range - creates an array of integers
+ * @min: mininum number
+ * @max: max number
+ * Return: pointer to newly created array
+ */
+int *array_range(int min, int max)
+{
+int *a;
+int s, i;
+if (min > max)
+return (NULL);
+s = max - min + 1;
+a = (int *)malloc(s * sizeof(int));
+if (a == NULL)
+return (NULL);
+for (i = 0; i < s; i++)
+a[i] = min++;
+return (a);
+}
