f3a0c8944982c384e59208d114ee18d4ef61e266
SilverDontAsk <steven_rosario@penguin>
2024-03-14 13:40:18 -0400

    Betty kinda weird here

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
-
+/*
+ * struct dog - data of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of dog owner
+ *
+ * Description: this structure represents information about a dog
+ */
 typedef struct dog
 {
-char *name;
-float age;
-char *owner;
+char *name; /* name of dog */
+float age; /* age of dog */
+char *owner; /* address of dog owner */
 } dog;
 int _putchar(char c);
-#endif
+#endif /* DOG_H */
