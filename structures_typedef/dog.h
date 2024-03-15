da98df9d0c7266de9e6171b5f63ad97ba397bdc2
SilverDontAsk <steven_rosario@penguin>
2024-03-15 13:27:17 -0400

    This should work now

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,12 +6,7 @@
  * @age: age of dog
  * @owner: name of dog's owner
  *
- * struct dog_t - Represents information about a dog
- * @name: name of dog
- * @age: age of dog
- * @owner: name of dog owner
- *
- * Description: this structure represents info about a dog,
+ * Description: these structures represents info about a dog,
  * including its age, name, and owner.
  */
 typedef struct dog
@@ -19,13 +14,8 @@ typedef struct dog
 char *name; /* name of dog */
 float age; /* age of dog */
 char *owner; /* address of dog owner */
-} dog;
-typedef struct dog_t
-{
-char *name; /*name of dog */
-float age; /*age of dog */
-char *owner; /* adress of dog owner */
 } dog_t;
+{
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
