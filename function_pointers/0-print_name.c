b3f9f5d7077cb9d45acd017363f64140f4964fec
SilverDontAsk <steven_rosario@penguin>
2024-03-18 11:19:57 -0400

    have everything, I just wanna see all requirements

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to function
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+
