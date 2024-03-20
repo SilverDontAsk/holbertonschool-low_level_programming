69439d40cb959d3190203384485f066f5a62b7ee
SilverDontAsk <steven_rosario@penguin>
2024-03-20 13:42:59 -0400

    eh

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,7 +12,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_list p;
 
 	va_start (p, n);
-	for (i = 0; i < n; i++)
+	for (i = 0; i <= n; i++)
 		s += va_arg(p, int);
 	va_end(p);
 	return (s);
