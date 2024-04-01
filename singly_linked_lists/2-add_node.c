85a0df8446b7875d5e2a8899d883f0a55532a63d
SilverDontAsk <steven_rosario@penguin>
2024-04-01 13:33:28 -0400

    This seems to work

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * add_node - adds a new node at the beginning of a linked list
+ * @head: pointer to pointer of head
+ * @str: string
+ * Return: address of the new element or NULL if fail
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *n_node = malloc(sizeof(list_t));
+if (n_node == NULL)
+{
+return (NULL);
+}
+n_node->str = strdup(str);
+if (n_node->str == NULL)
+{
+free(n_node);
+return (NULL);
+}
+n_node->len = strlen(str);
+n_node->next = *head;
+*head = n_node;
+return (n_node);
+}
