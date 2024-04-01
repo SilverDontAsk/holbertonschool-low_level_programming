a85e89ca8975fbe516c6e03e9a7e3593877fbeac
SilverDontAsk <steven_rosario@penguin>
2024-04-01 12:58:03 -0400

    betty fix

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,16 +8,16 @@
  */
 size_t print_list(const list_t *h)
 {
-	const list_t *current = h;
-	size_t c = 0;
-	while (current != NULL)
-	{
-		if (current->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", current->len, current->str);
-		current = current->next;
-		c++;
-	}
-	return (c);
+const list_t *current = h;
+size_t c = 0;
+while (current != NULL)
+{
+if (current->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%d] %s\n", current->len, current->str);
+current = current->next;
+c++;
+}
+return (c);
 }
