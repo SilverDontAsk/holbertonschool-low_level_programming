d401e4a85394824841fe7347b2baa66c83fc129b
SilverDontAsk <steven_rosario@penguin>
2024-04-01 13:37:02 -0400

    code fixes

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,10 +10,19 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+int len;
+const char *ptr;
 list_t *n_node = malloc(sizeof(list_t));
 if (n_node == NULL)
 {
 return (NULL);
+}
+len = 0;
+ptr = str;
+while(*ptr != '\0')
+{
+	len++;
+	ptr++;
 }
 n_node->str = strdup(str);
 if (n_node->str == NULL)
@@ -21,7 +30,7 @@ if (n_node->str == NULL)
 free(n_node);
 return (NULL);
 }
-n_node->len = strlen(str);
+n_node->len = len;
 n_node->next = *head;
 *head = n_node;
 return (n_node);
