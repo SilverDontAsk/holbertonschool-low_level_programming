7b46a2eba2f4478d38dc5101bb28d6ac5e21c829
SilverDontAsk <steven_rosario@penguin>
2024-04-02 10:33:08 -0400

    bruh

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -9,13 +9,13 @@
  */
 void free_list(list_t *head)
 {
-	list_t *current = head;
-	list_t *next;
-	while (current != NULL)
-	{
-		next = current->next;
-		free(current->str);
-		free(current);
-		current = next;
-	}
+list_t *current = head;
+list_t *next;
+while (current != NULL)
+{
+next = current->next;
+free(current->str);
+free(current);
+current = next;
+}
 }
