2cc6bcb7028034c5640459bd6abd834b94f06538
SilverDontAsk <steven_rosario@penguin>
2024-04-03 11:44:55 -0400

    this work

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_dlistint - prints a doubly linked list
+ * @h: pointer to head in list
+ * Return: nodes in list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t c = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		c++;
+	}
+	return (c);
+}
