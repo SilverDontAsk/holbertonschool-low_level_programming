32c6ed12ba4a292f2e1d7a4aba5da2d01e9a0079
SilverDontAsk <steven_rosario@penguin>
2024-04-03 13:39:09 -0400

    This works

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -9,14 +9,14 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	unsigned int i;
-	for (i = 0; head != NULL && i < index; i++)
-	{
-		head = head->next;
-	}
-	if (i == index)
-	{
-		return (head);
-	}
-	return (NULL);
+unsigned int i;
+for (i = 0; head != NULL && i < index; i++)
+{
+head = head->next;
+}
+if (i == index)
+{
+return (head);
+}
+return (NULL);
 }
