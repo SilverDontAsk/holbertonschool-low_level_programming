193b960ca527253f9fe8a15f6176370f5fc30cdf
SilverDontAsk <steven_rosario@penguin>
2024-04-04 10:12:47 -0400

    this works

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -2,37 +2,34 @@
 #include <stdlib.h>
 #include <stddef.h>
 /**
- * delete_dnodeint_at_index - 
- * @head: pointer to pointer to head 
+ * delete_dnodeint_at_index - deletes a node at a given position
+ * @head: pointer to pointer to head
  * @index: index of the node that should be deleted
  * Return: 1 if success, -1 if failure
  */
-
-
-
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current, *temp;
-	unsigned int k;
-	if (*head == NULL)
-		return (-1);
-	current = *head;
-	if (index == 0)
-	{
-		*head = current->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
-		free(current);
-		return (1);
-	}
-	for (k = 0; current != NULL && k < index - 1; k++)
-		current = current->next;
-	if (current == NULL || current->next == NULL)
-		return (-1);
-	temp = current->next->next;
-	free(current->next);
-	current->next = temp;
-	if (temp != NULL)
-		temp->prev = current;
-	return (1);
+dlistint_t *current, *temp;
+unsigned int k;
+if (*head == NULL)
+return (-1);
+current = *head;
+if (index == 0)
+{
+*head = current->next;
+if (*head != NULL)
+(*head)->prev = NULL;
+free(current);
+return (1);
+}
+for (k = 0; current != NULL && k < index - 1; k++)
+current = current->next;
+if (current == NULL || current->next == NULL)
+return (-1);
+temp = current->next->next;
+free(current->next);
+current->next = temp;
+if (temp != NULL)
+temp->prev = current;
+return (1);
 }
