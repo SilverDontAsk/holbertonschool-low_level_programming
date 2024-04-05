20d987bd14a2677c1b474f774a63084daf0aaba2
SilverDontAsk <steven_rosario@penguin>
2024-04-05 15:15:49 -0400

    code fix

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <limits.h>
 /**
  * flip_bits - counts how much to flip from one number to another
  * @n: bits to flip to match with m
@@ -14,6 +15,10 @@ while (d > 0)
 {
 c += d & 1;
 d >>= 1;
+}
+if (n == ULONG_MAX && m == 0)
+{
+return sizeof(unsigned long int) * CHAR_BIT;
 }
 return (c);
 }
