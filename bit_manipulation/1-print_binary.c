186b05bbae31bf537b1c8d1656c9a0e7fa6d75ae
SilverDontAsk <steven_rosario@penguin>
2024-04-05 11:33:01 -0400

    this should print the respective binary representation of a given number

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to turn into binaru
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+int f = 1;
+unsigned long int b = 1UL << (sizeof(n) * 8 - 1);
+if (n == 0)
+{
+	_putchar('0');
+	return;
+}
+while ((b & n) == 0)
+{
+	b >>= 1;
+	f++;
+}
+while (b != 0)
+{
+	_putchar((n & b) ? '1' : '0');
+	b >>= 1;
+}
+}
