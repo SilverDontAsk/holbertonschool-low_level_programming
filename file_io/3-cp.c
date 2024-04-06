2836b69c65ae0f0da73c273cb39ca0fe64e4a5a6
SilverDontAsk <steven_rosario@penguin>
2024-04-06 18:04:04 -0400

    idk

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,12 +4,7 @@
 #include <fcntl.h>
 #include <stdarg.h>
 #define BUFFER_SIZE 1024
-/**
- * p_error_exit - prints error message and exit with the given exit code
- * @exit_code: exit code to exit with
- * @format: how function is written
- * Return: nothing
- */
+
 void p_error_and_exit(int exit_code, const char *format, ...)
 {
 va_list args;
@@ -20,69 +15,67 @@ dprintf(2, "\n");
 va_end(args);
 exit(exit_code);
 }
-/**
- * copy_file - function to copy file
- * @src_file: source file to copy from
- * @dest_file: where to copy to
- * Return: nothing
- */
+int close_errchk(int fd)
+{
+int err = close(fd);
+if (err == -1) {
+p_error_and_exit(100, "Can't close file descriptor %d", fd);
+}
+return err;
+}
+void read_err(int src_fd, int dest_fd, const char *src_file)
+{
+close_errchk(src_fd);
+close_errchk(dest_fd);
+p_error_and_exit(98, "Can't read from file %s", src_file);
+}
+void write_err(int src_fd, int dest_fd, const char *dest_file)
+{
+close_errchk(src_fd);
+close_errchk(dest_fd);
+p_error_and_exit(99, "Can't write to file %s", dest_file);
+}
 void copy_file(const char *src_file, const char *dest_file)
 {
 int src_fd = open(src_file, O_RDONLY);
 int dest_fd = open(dest_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-char *b = malloc(BUFFER_SIZE);
+char *buffer = malloc(BUFFER_SIZE);
 ssize_t bytes_r, bytes_w;
-int e_code = 0;
-if (src_fd == -1)
-{
+if (src_fd == -1) {
 p_error_and_exit(98, "Can't read from file %s", src_file);
 }
-if (dest_fd == -1)
-{
-close(src_fd);
+if (dest_fd == -1) {
+close_errchk(src_fd);
 p_error_and_exit(99, "Can't write to file %s", dest_file);
 }
-if (b == NULL)
-{
-close(src_fd);
-close(dest_fd);
+if (buffer == NULL) {
+close_errchk(src_fd);
+close_errchk(dest_fd);
 p_error_and_exit(99, "Memory allocation failed");
 }
-while ((bytes_r = read(src_fd, b, BUFFER_SIZE)) > 0)
-{
-bytes_w = write(dest_fd, b, bytes_r);
-if (bytes_w == -1 || bytes_w != bytes_r)
-{
-e_code = 99;
-break;
+while ((bytes_r = read(src_fd, buffer, BUFFER_SIZE)) > 0) {
+bytes_w = write(dest_fd, buffer, bytes_r);
+if (bytes_w == -1 || bytes_w != bytes_r) {
+free(buffer);
+write_err(src_fd, dest_fd, dest_file);
 }
 }
-if (bytes_r == -1)
-{
-e_code = 98;
+if (bytes_r == -1) {
+free(buffer);
+read_err(src_fd, dest_fd, src_file);
 }
-free(b);
-close(src_fd);
-close(dest_fd);
-if (e_code != 0)
-{
-p_error_and_exit(e_code, "Error occurred during file copy");
+free(buffer);
+close_errchk(src_fd);
+close_errchk(dest_fd);
 }
-}
-/**
- * main - processes the arguments
- * @argc: argument count
- * @argv: argument vector
- * Return: copied files
- */
 int main(int argc, char *argv[])
 {
-const char *file_from = argv[1];
-const char *file_to = argv[2];
-if (argc != 3)
-{
+const char *file_from, *file_to;
+if (argc != 3) {
 p_error_and_exit(97, "Usage: %s file_from file_to", argv[0]);
 }
+file_from = argv[1];
+file_to = argv[2];
 copy_file(file_from, file_to);
-return (0);
+return 0;
 }
